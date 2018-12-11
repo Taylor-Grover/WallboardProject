c54090573b480ce19f98014d4b10649fd708d3ff
Taylor-Grover <taylor.j.grover@gmail.com>
2018-12-11 16:48:54 -0700

    modelos

--- WallboardSpecialties/Models/SubContractor.cs
+++ WallboardSpecialties/Models/SubContractor.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace WallboardSpecialties.Models
 {
+    [Table("Subcontractor")]
     public class SubContractor
     {
         //SubContractor Attributes
-
+        [Key]
         [DisplayName("Subcontractor ID")]
         public int subcontractorID { get; set; }
 

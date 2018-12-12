4f8027b7a484f1891396647078adb61cd5f58dfc
isaacloveschicken <isaacww@byu.edu>
2018-12-11 17:14:08 -0700

    Some authentication

--- WallboardSpecialties/Controllers/HomeController.cs
+++ WallboardSpecialties/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WallboardSpecialties.DAL;
+using WallboardSpecialties.Models;
 
 namespace WallboardSpecialties.Controllers
 {
     public class HomeController : Controller
     {
+        private WallboardContext db = new WallboardContext();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +30,35 @@ namespace WallboardSpecialties.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection form, bool rememberMe = false)
+        {
+            String email = form["Email address"].ToString();
+            String password = form["Password"].ToString();
+
+            var currentUser = db.Database.SqlQuery<Admin>("SELECT *" +
+                                                            "FROM Admin" +
+                                                            "WHERE Username = '" + email + "' AND " +
+                                                            "password = '" + password + "'");
+
+            if (currentUser.Count() > 0)
+            {
+                FormsAuthentication.SetAuthCookie(email, rememberMe);
+
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
